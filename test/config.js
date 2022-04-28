5ac7f3d690ff5e7e53c68733fd7315083aa8d374
edward-coombes-nuodb <edward.coombes@3ds.com>
2022-04-28 11:38:03 -0400

    Big Bang

---
+++ test/config.js
@@ -0,0 +1,15 @@
+// Copyright (c) 2022, NuoDB, Inc.
+// All rights reserved.
+//
+// Redistribution and use permitted under the terms of the 3-clause BSD license.
+
+const config = {
+  database: 'test',
+  hostname: 'localhost',
+  port: 48004,
+  user: 'dba',
+  password: 'dba',
+  schema: 'USER'
+}
+
+module.exports = config;
